97063bd3ad47af264f04ee33ad3f59c59323ba21
nik39n <40721920+nik39n@users.noreply.github.com>
2021-09-24 18:21:27 +0300

    sdf

--- You/example-app/config/filesystems.php
+++ You/example-app/config/filesystems.php
@@ -42,7 +42,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage/app',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
